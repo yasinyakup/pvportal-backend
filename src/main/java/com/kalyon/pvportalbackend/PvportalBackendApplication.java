4a58f8ae1f840772aa30a989c6c99e0a16f5c266
yasinyakup <yasinyakup@gmail.com>
2021-09-07 12:14:32 +0300

    initial commit

---
+++ src/main/java/com/kalyon/pvportalbackend/PvportalBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kalyon.pvportalbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PvportalBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PvportalBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kalyon/pvportalbackend/PvportalBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kalyon.pvportalbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PvportalBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
